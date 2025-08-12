7b7e8c7a64274ea2aaf8daf45e22e9c28732de40
milimalism <mythili.mulani17@gmail.com>
2025-08-12 17:38:50 +0800

    open url functionality

--- read_qr/ContentView.swift
+++ read_qr/ContentView.swift
@@ -11,17 +11,21 @@ import Vision
 
 struct ContentView: View {
     @State private var image = NSImage()
-    @State private var url: URL? = nil
+    @State private var qrURL: URL? = nil
 
     @Environment(\.openURL) private var openURL
 
     var body: some View {
         VStack {
             Button {
+                qrURL = nil
                 if ScreenshotQRCode(){
                     image = RetrieveImage()
                     print("taken screenshot")
-                    decodeQrFromImage(from: image)
+                    if let qrString = decodeQrFromImage(from: image),
+                       let url = URL(string: qrString) {
+                        qrURL = url
+                    }
                 }
             } label: {
                 Text("Select QR code")
@@ -30,6 +34,15 @@ struct ContentView: View {
             Text("Captured QR")
             
             Image(nsImage: image)
+            
+            Button {
+                if let url = qrURL {
+                    openURL(url)
+                }
+            } label: {
+                Text("Open url!")
+            }
+            .disabled(qrURL == nil)
         }.padding()
         
     }
@@ -61,25 +74,29 @@ struct ContentView: View {
     //    2. Receive the results of the qr detection request as arrays of VNBarcodeObservation objects.
     //    The VNBarcodeObservation object contains:
     //      An recognized payloadString (payloadStringValue) <- the decoding we want!!!
-    //    3. Use a handler to create a request and send a request for qr detection.
+    //    3. Use a handler to create a request and send a request for qr detection
 
     //
     //
     //    Create a request for image processing, and send a request for text recognition.
-        func decodeQrFromImage(from image: NSImage) {
+    func decodeQrFromImage(from image: NSImage) -> String? {
             guard let cgImage = image.cgImage(forProposedRect: nil, context: nil, hints: nil) else {
                     print("Could not get CGImage from NSImage")
-                    return
+                    return nil
                 }
             print("got image")
+        
+            var qrLink: String? = nil
 
             let request = VNDetectBarcodesRequest { request, error in // 1
                 guard let observations = request.results as? [VNBarcodeObservation] else { return } // 2
 
                 for observation in observations {
                     if let payload = observation.payloadStringValue {
-                                    print("QR Code String: \(payload)")
-                                }
+                            print("QR Code String: \(payload)")
+                            qrLink = payload
+                            return
+                        }
                 }
             }
 
@@ -87,6 +104,8 @@ struct ContentView: View {
 
             let handler = VNImageRequestHandler(cgImage: cgImage, options: [:]) // 3
             try? handler.perform([request]) // 3
+        
+            return qrLink
         }
     
 }
